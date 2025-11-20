9c440912261d0295c9fbe5dc719255846f067d86
rohith-36 <136504575+rohith-36@users.noreply.github.com>
2025-11-20 17:45:59 +0530

    Create RouteConfig.cs

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace StudentManagementSystem
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Default route
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Students", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
